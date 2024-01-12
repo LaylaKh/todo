cd1260589322e568ce8dd5cd6ddc295c415a30d7
LaylaKh <layla.khdva@gmail.com>
2024-01-12 17:46:43 +0300

    first commit

---
+++ lib/widgets/groups/goups_widget.dart
@@ -0,0 +1,100 @@
+import 'package:dart_lesson/widgets/groups/goups_widget_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+
+class GroupsWidget extends StatefulWidget {
+  const GroupsWidget({Key? key}) : super(key: key);
+
+  @override
+  _GroupsWidgetState createState() => _GroupsWidgetState();
+}
+
+class _GroupsWidgetState extends State<GroupsWidget> {
+  final model = GroupsWidgetModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return GroupsWidgetModelProvider(
+      model: model,
+      child: const _GroupsWidgetBody(),
+    );
+  }
+}
+
+class _GroupsWidgetBody extends StatelessWidget {
+  const _GroupsWidgetBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Группы'),
+      ),
+      body: const _GroupListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            GroupsWidgetModelProvider.read(context)?.model.showFrom(context),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _GroupListWidget extends StatelessWidget {
+  const _GroupListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final groupsCount =
+        GroupsWidgetModelProvider.watch(context)?.model.groups.length ?? 0;
+    return ListView.separated(
+      itemCount: groupsCount,
+      itemBuilder: (BuildContext context, int index) {
+        return _GroupListRowWidget(indexInList: index);
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider(height: 1);
+      },
+    );
+  }
+}
+
+class _GroupListRowWidget extends StatelessWidget {
+  final int indexInList;
+  const _GroupListRowWidget({
+    Key? key,
+    required this.indexInList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = GroupsWidgetModelProvider.read(context)!.model;
+    final group = model.groups[indexInList];
+
+    return Slidable(
+      endActionPane:
+        ActionPane(
+        motion: const ScrollMotion(),
+         children: [
+          SlidableAction(
+            onPressed: (context){},
+            backgroundColor: const Color.fromARGB(255, 233, 13, 13),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+           )
+        ]
+      ),
+      child: ColoredBox(
+        color: Colors.white,
+        child: ListTile(
+          title: Text(group.name),
+          trailing: const Icon(Icons.chevron_right),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
+
